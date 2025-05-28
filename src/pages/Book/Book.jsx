6cf71f1bf2917e6bf93660ffc9e9d37af4168241
SHANTO <rabbymd55555@gmail.com>
2025-05-29 00:25:09 +0600

    added navbar and footer

--- src/pages/Book/Book.jsx
+++ src/pages/Book/Book.jsx
@@ -1,22 +1,20 @@
 import React, { use } from "react";
+import { FaStarHalfAlt } from "react-icons/fa";
 
 const Book = ({ ho }) => {
   // const book = use(bookPromise)
   // console.log(book);
 
-  const {bookName , author,image} = ho
+  const { bookName, author, image, rating ,category} = ho;
   return (
     <div>
-      <div className="card bg-base-100 w-96 shadow-sm">
-        <figure>
-          <img className="w-[200px] h-[200px]"
-            src={image}
-            alt="Shoes"
-          />
+      <div className="card bg-base-100  shadow-sm border p-5">
+        <figure className="p-5 bg-gray-200">
+          <img className="w-[200px] h-[200px]" src={image} alt="Shoes" />
         </figure>
         <div className="card-body">
           <h2 className="card-title">
-            Card Title
+            {bookName}
             <div className="badge badge-secondary">NEW</div>
           </h2>
           <p>
@@ -24,8 +22,9 @@ const Book = ({ ho }) => {
             are title and actions parts
           </p>
           <div className="card-actions justify-end">
-            <div className="badge badge-outline">Fashion</div>
-            <div className="badge badge-outline">Products</div>
+            <div className="">{category}</div>
+            <div className="">{rating}</div>
+            <FaStarHalfAlt></FaStarHalfAlt>
           </div>
         </div>
       </div>
