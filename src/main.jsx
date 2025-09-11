45334c4383aa93a51ec36881a7f0497f7cae0d59
SHANTO <rabbymd55555@gmail.com>
2025-09-11 15:35:10 +0600

    boi poka

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,15 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { RouterProvider } from "react-router";
-import { router } from "./Routes/Routes.jsx";
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello World</div>,
+  },
+]);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
