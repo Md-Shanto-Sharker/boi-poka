5e91f0a739bec7b4a315322d28e6bd25077520fa
SHANTO <rabbymd55555@gmail.com>
2025-09-11 20:10:38 +0600

    boi poka navbar and footer

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,16 @@
+import { createBrowserRouter } from "react-router";
+import Root from "../Pages/Root/Root";
+import Errorpage from "../Pages/Errorpage/Errorpage";
+import Home from "../Pages/Home/Home";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: Root,
+    errorElement:<Errorpage></Errorpage>,
+    children: [
+      { index: true, path:'/', Component: Home },
+    //   { path: "about", Component: About },
+    ],
+  },
+]);
