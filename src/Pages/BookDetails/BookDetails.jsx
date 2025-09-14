497d0824fa7f16f5f2e4569e00416f3772c81b08
SHANTO <rabbymd55555@gmail.com>
2025-09-14 18:52:58 +0600

    added local storage

--- src/Pages/BookDetails/BookDetails.jsx
+++ src/Pages/BookDetails/BookDetails.jsx
@@ -1,4 +1,5 @@
 import { useLoaderData, useParams } from "react-router";
+import { addToStoredDB } from "../../utility/AddToDB";
 
 const BookDetails = () => {
   const { id } = useParams();
@@ -6,13 +7,17 @@ const BookDetails = () => {
   const data = useLoaderData();
   const singleBook = data.find((book) => book.bookId === bookId);
   const { bookName, image } = singleBook;
+
+  const handleMarkAsRead = (id) => {
+    addToStoredDB(id)
+  };
   return (
     <div className="border-2 w-2/3">
       <img className="w-48" src={image} alt="" />
       <h5>{bookName}</h5>
 
-      <button className="btn btn-accent m-2">Read</button>
-      <button className="btn btn-info m-2">WishList</button>
+      <button onClick={()=>handleMarkAsRead(id)} className="btn btn-accent m-2">Mark as Read</button>
+      <button className="btn btn-info m-2">Add To WishList</button>
     </div>
   );
 };
